14527bceb0c43a2138e836dc418aae6182fe16d8
Daniel Hoffman <dj.hoffman@delphix.com>
2016-04-21 08:47:59 -0700

    DLPX-36742 upgrade to open-vm-tools 10.0.5 (fix copyright)

--- build/open-vm-tools/build.sh
+++ build/open-vm-tools/build.sh
@@ -22,7 +22,7 @@
 #
 
 #
-# Copyright (c) 2014 by Delphix. All rights reserved.
+# Copyright (c) 2014, 2016 by Delphix. All rights reserved.
 #
 
 # Load support functions
--- lib/functions.sh
+++ lib/functions.sh
@@ -23,7 +23,7 @@
 #
 # Copyright 2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
-# Copyright (c) 2014, 2015 by Delphix. All rights reserved.
+# Copyright (c) 2014, 2016 by Delphix. All rights reserved.
 #
 
 umask 022
